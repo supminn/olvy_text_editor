71fb61cec56a61c3ea846273fe069627a9e1a408
supminn <30731236+supminn@users.noreply.github.com>
2021-08-01 22:02:45 +0530

    refactor: button component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,9 @@ import { Button } from "react-bootstrap";
 export const NavBar = ({ setShow }) => {
   return (
     <nav className="border border-dark bg-dark text-white px-2 d-flex justify-content-between align-items-center position-sticky top-0">
-      <button className="btn text-light fs-4 fst-italic">Richie Editor</button>
+      <Button variant="dark" className="fs-4 fst-italic">
+        Richie Editor
+      </Button>
       <Button variant="outline-light" onClick={() => setShow(true)}>
         How to use?
       </Button>
