fdcb2cc3895e9b7aadddbafaf17d8baba46f6a52
supminn <30731236+supminn@users.noreply.github.com>
2021-08-02 10:31:34 +0530

    fix: image width, footer position

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <footer className="border border-dark bg-dark text-white p-2 text-center">
+    <footer className="footer border border-dark bg-dark text-white p-2 text-center">
       <h5 className="text-secondary">
         Made with <i className="fas fa-heart text-danger"></i> by{" "}
         <em className="text-white">Supminn</em>
